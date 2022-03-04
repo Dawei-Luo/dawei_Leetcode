050a6559cc03612a996cb397287744ba5ce9ea8a
Dawei <daweiluo1112@gmail.com>
2022-03-04 15:19:20 +0800

    03.Longest Substring Without Repeating Characters

---
+++ Longest Substring Without Repeating Characters_03/Program.cs
@@ -0,0 +1,23 @@
+﻿string s = "abcabcbb";
+var words = new Dictionary<string, bool> { };
+int len = 0;
+int left = 0;
+
+for (int i = 0; i < s.Length; i++)
+{
+    string str = s[i].ToString();
+    if (!words.ContainsKey(str))
+    {
+        words.Add(str, true);
+    }
+    else
+    {
+        len = Math.Max(words.Count, len);
+        words.Clear();
+        i = left++;
+    }
+
+}
+len = Math.Max(words.Count, len);
+
+Console.WriteLine(len);
