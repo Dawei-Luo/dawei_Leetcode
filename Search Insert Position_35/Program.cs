3fb7450092f40b8a11a2e7b5c0ba21b0453de7d5
Dawei <daweiluo1112@gmail.com>
2022-03-04 15:22:14 +0800

    35. Search Insert Position

---
+++ Search Insert Position_35/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] nums = { 1, 3, 5, 6 };
+int target = 2;
+
+
+int l = 0, r = nums.Length - 1;
+int mid = -1;
+while(l<=r)
+{
+    mid = (l + r) / 2;
+    if (target == nums[mid])
+    {
+        Console.WriteLine(mid);
+    }
+    if (target > nums[mid])
+    {
+        l = mid + 1;
+    }
+    else
+    {
+        
+        r = mid - 1;
+    }
+    
+}
+Console.WriteLine(l);
+
+
+
